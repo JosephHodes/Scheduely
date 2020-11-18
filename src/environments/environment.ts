1d012c5949dc33d4f40758550549b10a06aae519
joseph <jojoho70@gmail.com>
2020-11-18 16:39:04 -0500

    man

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false
 };
 
-/*
+/*get
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.igitnvokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
